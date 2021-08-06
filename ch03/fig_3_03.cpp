3dfaf1c9626790e78bca40a9df6b726deebe20e8
Aaron Jouvenat <aaron.jouvenat@ecstech.com>
2021-08-06 10:00:41 -0400

    Update ch3 to work with tbb 2021.3

--- ch03/fig_3_03.cpp
+++ ch03/fig_3_03.cpp
@@ -56,7 +56,7 @@ void fig_3_3() {
 }
 
 static void warmupTBB() {
-  tbb::parallel_for(0, tbb::task_scheduler_init::default_num_threads(), [](int) {
+  tbb::parallel_for(0, tbb::this_task_arena::max_concurrency(), [](int) {
     tbb::tick_count t0 = tbb::tick_count::now();
     while ((tbb::tick_count::now() - t0).seconds() < 0.01);
   });
